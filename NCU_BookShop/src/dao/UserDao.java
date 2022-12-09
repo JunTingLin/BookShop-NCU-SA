06e81f0b0ee8a87e2655d230baf6edb647784351
junting <10355098@st.thsh.tp.edu.tw>
2022-12-10 01:21:24 +0800

    完整去除isvalidate

--- NCU_BookShop/src/dao/UserDao.java
+++ NCU_BookShop/src/dao/UserDao.java
@@ -13,8 +13,8 @@ import java.util.List;
 public class UserDao {
     public void addUser(User user) throws SQLException {
         QueryRunner r = new QueryRunner(DBUtil.getDataSource());
-        String sql = "insert into user(username,email,password,name,phone,address,isadmin,isvalidate) values(?,?,?,?,?,?,?,?)";
-        r.update(sql,user.getUsername(),user.getEmail(),user.getPassword(),user.getName(),user.getPhone(),user.getAddress(),user.isIsadmin(),user.isIsvalidate());
+        String sql = "insert into user(username,email,password,name,phone,address,isadmin) values(?,?,?,?,?,?,?)";
+        r.update(sql,user.getUsername(),user.getEmail(),user.getPassword(),user.getName(),user.getPhone(),user.getAddress(),user.isIsadmin());
     }
     public boolean isUsernameExist(String username) throws SQLException {
         QueryRunner r = new QueryRunner(DBUtil.getDataSource());
