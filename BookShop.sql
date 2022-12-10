56ae0e547740bedcaaeaf30caf5ec6c9486f1af9
junting <10355098@st.thsh.tp.edu.tw>
2022-12-10 13:55:02 +0800

    dump sql file update

--- BookShop.sql
+++ BookShop.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50739
  File Encoding         : 65001
 
- Date: 05/12/2022 02:25:54
+ Date: 10/12/2022 13:54:43
 */
 
 SET NAMES utf8mb4;
@@ -39,12 +39,12 @@ CREATE TABLE `goods`  (
 -- ----------------------------
 -- Records of goods
 -- ----------------------------
-INSERT INTO `goods` VALUES (15, 'Leo Tolstoy, Anna Karenina (Rosamund Bartlett', '/picture/15-1.jpg', '/picture/15-1.jpg', '/picture/15-1.jpg', 300, '女性主義專題Ａ-本課程立意以「表演藝術」作為社會實踐與人文批判的教學策略，透過業界專業師資的引進，嘗試建立一套表演藝術教學實作教學系統，引導學生在藝術創作的實踐中， 提升自主學習能力並建立跨域合作方法。', 8, 1);
-INSERT INTO `goods` VALUES (25, 'Classical mechanics by John R. Taylor', '/picture/25-1.jpg', '/picture/25-1.jpg', '/picture/25-1.jpg', 38, '力學- By analyzing classical mechanical systems with vector calculus and linear algebra, the main objective of this course is to prepare the students for more advanced physics courses. ', 10, 2);
+INSERT INTO `goods` VALUES (15, 'Leo Tolstoy, Anna Karenina (Rosamund Bartlett', '/picture/15-1.jpg', '/picture/15-1.jpg', '/picture/15-1.jpg', 300, '女性主義專題Ａ-本課程立意以「表演藝術」作為社會實踐與人文批判的教學策略，透過業界專業師資的引進，嘗試建立一套表演藝術教學實作教學系統，引導學生在藝術創作的實踐中， 提升自主學習能力並建立跨域合作方法。', 0, 1);
+INSERT INTO `goods` VALUES (25, 'Classical mechanics by John R. Taylor', '/picture/25-1.jpg', '/picture/25-1.jpg', '/picture/25-1.jpg', 38, '力學- By analyzing classical mechanical systems with vector calculus and linear algebra, the main objective of this course is to prepare the students for more advanced physics courses. ', 3, 2);
 INSERT INTO `goods` VALUES (40, 'Erwin Kreyszig, “Advanced Engineering Mathema', '/picture/40-1.jpg', '/picture/40-1.jpg', '/picture/40-1.jpg', 1800, ' 工程數學 Ｉ-本課程教學目標主要在於訓練學生解決工程相關問題的數學能力，讓學生能夠從較實務性的工程相關問題和物理問題中，理解它們和數學上的相關性。藉由本課程中學得的數學能力，以解決工程上相關的問題。', 10, 3);
-INSERT INTO `goods` VALUES (55, '計算機概論(第四版)(Forouzan 4/e)', '/picture/55.jpg', '/picture/55.jpg', '/picture/55.jpg', 36, 'IM1001-* 計算機概論Ｉ本課程的主要目標在對大一同學介紹與資管領域相關與需要認識的電腦科技， 使同學對這些科技能有基本概念， 以做為未來各個相關課程的基礎。因此，本課程將提供廣度的多領域介紹。為達成上述目標，本課程內容包括：(1)介紹電腦的整體架構，以對電腦有整體概念。', 10, 4);
+INSERT INTO `goods` VALUES (55, '計算機概論(第四版)(Forouzan 4/e)', '/picture/55.jpg', '/picture/55.jpg', '/picture/55.jpg', 36, 'IM1001-* 計算機概論Ｉ本課程的主要目標在對大一同學介紹與資管領域相關與需要認識的電腦科技， 使同學對這些科技能有基本概念， 以做為未來各個相關課程的基礎。因此，本課程將提供廣度的多領域介紹。為達成上述目標，本課程內容包括：(1)介紹電腦的整體架構，以對電腦有整體概念。', 7, 4);
 INSERT INTO `goods` VALUES (82, ' Handbook of Applied Cryptography by Alfred J', '/picture/82.jpg', '/picture/82.jpg', '/picture/82.jpg', 299, '這門課程旨在用科技與社會的角度去理解資安與隱私的重要性，如何用賽局的角度去理解敵人，如何設計安全穩定的系統。此課程重點為用寬廣的角度去理解世界上各式各樣的資安問題，並不著重深入討論資安相關技術，例如:駭客、CODING、資安管理等。', 10, 5);
-INSERT INTO `goods` VALUES (114, ' Mesoscale Meteorology in Midlatitudes , Mark', '/picture/114.jpg', '/picture/114.jpg', '/picture/114.jpg', 28, '本課程目的為介紹中尺度天氣現象, 並就各種不同的中尺度天氣系統, 說明它們的生成機制、內部的結構、物理與動力過程、維持機制等。透過專題報告的方式，幫助學生了解實際中尺度天氣個案。', 10, 6);
+INSERT INTO `goods` VALUES (114, ' Mesoscale Meteorology in Midlatitudes , Mark', '/picture/114.jpg', '/picture/114.jpg', '/picture/114.jpg', 28, '本課程目的為介紹中尺度天氣現象, 並就各種不同的中尺度天氣系統, 說明它們的生成機制、內部的結構、物理與動力過程、維持機制等。透過專題報告的方式，幫助學生了解實際中尺度天氣個案。', 9, 6);
 
 -- ----------------------------
 -- Table structure for order
@@ -64,13 +64,15 @@ CREATE TABLE `order`  (
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `fk_user_id_idx`(`user_id`) USING BTREE,
   CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE = InnoDB AUTO_INCREMENT = 77 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 80 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of order
 -- ----------------------------
 INSERT INTO `order` VALUES (75, 28, 1, 4, 2, '管理員', '1333333333', '美國', '2022-10-07 12:31:07', 1);
-INSERT INTO `order` VALUES (76, 600, 2, 0, 1, 'vili', '1344444444', '美國', '2022-12-03 13:31:29', 2);
+INSERT INTO `order` VALUES (76, 600, 2, 1, 1, 'vili', '1344444444', '美國', '2022-12-03 13:31:29', 2);
+INSERT INTO `order` VALUES (78, 72, 2, 3, 1, '林俊霆', '電話不給', '地址不想給', '2022-12-10 13:40:21', 41);
+INSERT INTO `order` VALUES (79, 64, 2, 2, 2, 'vili', '1344444444', '美國', '2022-12-10 13:49:45', 2);
 
 -- ----------------------------
 -- Table structure for orderitem
@@ -87,13 +89,16 @@ CREATE TABLE `orderitem`  (
   INDEX `fk_orderitem_goods_id_idx`(`goods_id`) USING BTREE,
   CONSTRAINT `fk_order_id` FOREIGN KEY (`order_id`) REFERENCES `order` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `fk_orderitem_goods_id` FOREIGN KEY (`goods_id`) REFERENCES `goods` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE = InnoDB AUTO_INCREMENT = 80 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 84 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of orderitem
 -- ----------------------------
 INSERT INTO `orderitem` VALUES (78, 28, 1, 55, 75);
 INSERT INTO `orderitem` VALUES (79, 300, 2, 15, 76);
+INSERT INTO `orderitem` VALUES (81, 36, 2, 55, 78);
+INSERT INTO `orderitem` VALUES (82, 28, 1, 114, 79);
+INSERT INTO `orderitem` VALUES (83, 36, 1, 55, 79);
 
 -- ----------------------------
 -- Table structure for recommend
@@ -151,17 +156,16 @@ CREATE TABLE `user`  (
   `phone` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `address` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `isadmin` bit(1) NULL DEFAULT NULL,
-  `isvalidate` bit(1) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE,
   UNIQUE INDEX `username_UNIQUE`(`username`) USING BTREE,
   UNIQUE INDEX `email_UNIQUE`(`email`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 41 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 42 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES (1, 'admin', 'admin@gmail.com', '$2a$10$FdNRmAeG9VTPy7.m7.CXmu5opagl1v876xqHS6n8dTW1/6SK1blra', '管理員', '1333333333', '美國', b'1', b'0');
-INSERT INTO `user` VALUES (2, 'vili', 'vili@gmail.com', '$2a$10$duKyYeM3FEQ9z3IS9qxhlu71vRaCCQvePb0ZtkfJVsXgGiQY4Rsyq', 'vili', '1344444444', '美國', b'0', b'0');
-INSERT INTO `user` VALUES (40, 'junting', '10355098@st.thsh.tp.edu.tw', '$2a$10$ve.S7jjICxHcrsZfGyW4CORQK1e77Dk0ySGWBm4khfqgsVnKwRrSi', '', '', '', b'0', b'0');
+INSERT INTO `user` VALUES (1, 'admin', 'admin@gmail.com', '$2a$10$FdNRmAeG9VTPy7.m7.CXmu5opagl1v876xqHS6n8dTW1/6SK1blra', '管理員', '1333333333', '美國', b'1');
+INSERT INTO `user` VALUES (2, 'vili', 'vili@gmail.com', '$2a$10$duKyYeM3FEQ9z3IS9qxhlu71vRaCCQvePb0ZtkfJVsXgGiQY4Rsyq', 'vili', '1344444444', '美國', b'0');
+INSERT INTO `user` VALUES (41, 'junting', 'junting@ncu.edu.tw', '$2a$10$G1gDeimORybHlXLOqVgnBe/sZdR1/LjR28FNikFdKFT2IjoaWMDJm', '', '', '', b'0');
 
 SET FOREIGN_KEY_CHECKS = 1;
