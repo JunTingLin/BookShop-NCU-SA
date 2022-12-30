f537d285c64029a01580f7ea200c77fea4d04c3d
junting <10355098@st.thsh.tp.edu.tw>
2022-12-30 17:45:47 +0800

    update sql

--- BookShop.sql
+++ BookShop.sql
@@ -11,20 +11,22 @@
  Target Server Version : 50739
  File Encoding         : 65001
 
- Date: 28/12/2022 18:05:11
+ Date: 30/12/2022 17:45:27
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
--- Table structure for goods
+-- Table structure for books
 -- ----------------------------
-DROP TABLE IF EXISTS `goods`;
-CREATE TABLE `goods`  (
+DROP TABLE IF EXISTS `books`;
+CREATE TABLE `books`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `cover` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `image1` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `image2` varchar(45) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `price` int(11) NULL DEFAULT NULL,
   `intro` varchar(300) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
   `stock` int(11) NULL DEFAULT NULL,
@@ -32,17 +34,17 @@ CREATE TABLE `goods`  (
   PRIMARY KEY (`id`) USING BTREE,
   INDEX `fk_type_id_idx`(`type_id`) USING BTREE,
   CONSTRAINT `fk_type_id` FOREIGN KEY (`type_id`) REFERENCES `type` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE = InnoDB AUTO_INCREMENT = 115 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 116 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
--- Records of goods
+-- Records of books
 -- ----------------------------
-INSERT INTO `goods` VALUES (15, 'Leo Tolstoy, Anna Karenina (Rosamund Bartlett', '/picture/15-1.jpg', 300, '女性主義專題Ａ-本課程立意以「表演藝術」作為社會實踐與人文批判的教學策略，透過業界專業師資的引進，嘗試建立一套表演藝術教學實作教學系統，引導學生在藝術創作的實踐中， 提升自主學習能力並建立跨域合作方法。', 0, 1);
-INSERT INTO `goods` VALUES (25, 'Classical mechanics by John R. Taylor', '/picture/25-1.jpg', 38, '力學- By analyzing classical mechanical systems with vector calculus and linear algebra, the main objective of this course is to prepare the students for more advanced physics courses. ', 3, 2);
-INSERT INTO `goods` VALUES (40, 'Erwin Kreyszig, “Advanced Engineering Mathema', '/picture/40-1.jpg', 1800, ' 工程數學 Ｉ-本課程教學目標主要在於訓練學生解決工程相關問題的數學能力，讓學生能夠從較實務性的工程相關問題和物理問題中，理解它們和數學上的相關性。藉由本課程中學得的數學能力，以解決工程上相關的問題。', 10, 3);
-INSERT INTO `goods` VALUES (55, '計算機概論(第四版)(Forouzan 4/e)', '/picture/55.jpg', 36, 'IM1001-* 計算機概論Ｉ本課程的主要目標在對大一同學介紹與資管領域相關與需要認識的電腦科技， 使同學對這些科技能有基本概念， 以做為未來各個相關課程的基礎。因此，本課程將提供廣度的多領域介紹。為達成上述目標，本課程內容包括：(1)介紹電腦的整體架構，以對電腦有整體概念。', 7, 4);
-INSERT INTO `goods` VALUES (82, ' Handbook of Applied Cryptography by Alfred J', '/picture/82.jpg', 299, '這門課程旨在用科技與社會的角度去理解資安與隱私的重要性，如何用賽局的角度去理解敵人，如何設計安全穩定的系統。此課程重點為用寬廣的角度去理解世界上各式各樣的資安問題，並不著重深入討論資安相關技術，例如:駭客、CODING、資安管理等。', 10, 5);
-INSERT INTO `goods` VALUES (114, ' Mesoscale Meteorology in Midlatitudes , Mark', '/picture/114.jpg', 28, '本課程目的為介紹中尺度天氣現象, 並就各種不同的中尺度天氣系統, 說明它們的生成機制、內部的結構、物理與動力過程、維持機制等。透過專題報告的方式，幫助學生了解實際中尺度天氣個案。', 9, 6);
+INSERT INTO `books` VALUES (15, 'Leo Tolstoy, Anna Karenina (Rosamund Bartlett', '/picture/15-1.jpg', '/picture/15-1.jpg', '/picture/15-1.jpg', 300, '女性主義專題Ａ-本課程立意以「表演藝術」作為社會實踐與人文批判的教學策略，透過業界專業師資的引進，嘗試建立一套表演藝術教學實作教學系統，引導學生在藝術創作的實踐中， 提升自主學習能力並建立跨域合作方法。', 0, 1);
+INSERT INTO `books` VALUES (25, 'Classical mechanics by John R. Taylor', '/picture/25-1.jpg', '/picture/25-1.jpg', '/picture/25-1.jpg', 38, '力學- By analyzing classical mechanical systems with vector calculus and linear algebra, the main objective of this course is to prepare the students for more advanced physics courses. ', 3, 2);
+INSERT INTO `books` VALUES (40, 'Erwin Kreyszig, “Advanced Engineering Mathema', '/picture/40-1.jpg', '/picture/40-1.jpg', '/picture/40-1.jpg', 1800, ' 工程數學 Ｉ-本課程教學目標主要在於訓練學生解決工程相關問題的數學能力，讓學生能夠從較實務性的工程相關問題和物理問題中，理解它們和數學上的相關性。藉由本課程中學得的數學能力，以解決工程上相關的問題。', 10, 3);
+INSERT INTO `books` VALUES (55, '計算機概論(第四版)(Forouzan 4/e)', '/picture/55.jpg', '/picture/55.jpg', '/picture/55.jpg', 36, 'IM1001-* 計算機概論Ｉ本課程的主要目標在對大一同學介紹與資管領域相關與需要認識的電腦科技， 使同學對這些科技能有基本概念， 以做為未來各個相關課程的基礎。因此，本課程將提供廣度的多領域介紹。為達成上述目標，本課程內容包括：(1)介紹電腦的整體架構，以對電腦有整體概念。', 7, 4);
+INSERT INTO `books` VALUES (82, ' Handbook of Applied Cryptography by Alfred J', '/picture/82.jpg', '/picture/82.jpg', '/picture/82.jpg', 299, '這門課程旨在用科技與社會的角度去理解資安與隱私的重要性，如何用賽局的角度去理解敵人，如何設計安全穩定的系統。此課程重點為用寬廣的角度去理解世界上各式各樣的資安問題，並不著重深入討論資安相關技術，例如:駭客、CODING、資安管理等。', 10, 5);
+INSERT INTO `books` VALUES (114, ' Mesoscale Meteorology in Midlatitudes , Mark', '/picture/114.jpg', '/picture/114.jpg', '/picture/114.jpg', 28, '本課程目的為介紹中尺度天氣現象, 並就各種不同的中尺度天氣系統, 說明它們的生成機制、內部的結構、物理與動力過程、維持機制等。透過專題報告的方式，幫助學生了解實際中尺度天氣個案。', 9, 6);
 
 -- ----------------------------
 -- Table structure for order
@@ -86,7 +88,7 @@ CREATE TABLE `orderitem`  (
   INDEX `fk_order_id_idx`(`order_id`) USING BTREE,
   INDEX `fk_orderitem_goods_id_idx`(`goods_id`) USING BTREE,
   CONSTRAINT `fk_order_id` FOREIGN KEY (`order_id`) REFERENCES `order` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `fk_orderitem_goods_id` FOREIGN KEY (`goods_id`) REFERENCES `goods` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `fk_orderitem_goods_id` FOREIGN KEY (`goods_id`) REFERENCES `books` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE = InnoDB AUTO_INCREMENT = 84 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
@@ -105,10 +107,10 @@ DROP TABLE IF EXISTS `recommend`;
 CREATE TABLE `recommend`  (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `type` tinyint(1) NULL DEFAULT NULL,
-  `goods_id` int(11) NULL DEFAULT NULL,
+  `books_id` int(11) NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE,
-  INDEX `fk_goods_id_idx`(`goods_id`) USING BTREE,
-  CONSTRAINT `fk_goods_id` FOREIGN KEY (`goods_id`) REFERENCES `goods` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
+  INDEX `fk_goods_id_idx`(`books_id`) USING BTREE,
+  CONSTRAINT `fk_goods_id` FOREIGN KEY (`books_id`) REFERENCES `books` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE = InnoDB AUTO_INCREMENT = 15 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
