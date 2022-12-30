87794a955fa88fac514206e105877362e6b72024
junting <10355098@st.thsh.tp.edu.tw>
2022-12-30 19:01:17 +0800

    key set、value set

--- NCU_BookShop/src/main/java/servlet/IndexServlet.java
+++ NCU_BookShop/src/main/java/servlet/IndexServlet.java
@@ -18,10 +18,12 @@ public class IndexServlet extends HttpServlet {
     }
     private BooksService bService=new BooksService();
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        Map<String,Object> ScrollBook=bService.getScrollBook();
-        request.setAttribute("scroll",ScrollBook);
+//        Map<String,Object> ScrollBook=bService.getScrollBook();
+//        request.setAttribute("scroll",ScrollBook);
 
         List<Map<String,Object>>scrollList=bService.getBooksList(1);
+        System.out.println("這是key集合"+scrollList.get(0).keySet());
+        System.out.println("這是values"+scrollList.get(0).values());
         request.setAttribute("scrollList",scrollList);
 
         List<Map<String,Object>>newList=bService.getBooksList(2);
